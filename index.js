b692a4439f9eb110724ca3566fef3b25635963df
Timo <timo.hegnauer@neon-free.ch>
2021-02-09 13:37:26 +0100

    put all of it in one object

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let filename = 'countries_it.json'
+let filename = 'countries_fr.json'
 const fs = require('fs');
 let input = fs.readFileSync(filename);
 let parsedInput = JSON.parse(input);
@@ -11,10 +11,17 @@ for (let index = 0; index < parsedInput.length; index++) {
 	delete parsedInput[index].alpha3;
 	parsedInput[index].alpha2 = parsedInput[index].alpha2.toUpperCase()
 }
-
 // rename properties
-var resultArray = parsedInput.map(function (elm) {
-	return { value: elm.alpha2, text: elm.name };
+let resultArray = parsedInput.map(function (elm) {
+	// change this
+	return { [elm.alpha2]: elm.name };
 });
-let data = JSON.stringify(resultArray);
+
+// put all data in the same object
+let oneObject = {};
+for (var i = 0; i < resultArray.length; i++) {
+	oneObject[Object.keys(resultArray[i])] = resultArray[i][Object.keys(resultArray[i])];
+}
+
+let data = JSON.stringify(oneObject);
 fs.writeFileSync(`${filename}_output.json`, data);
